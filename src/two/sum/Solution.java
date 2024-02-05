c0647c4bb37786d768b6987708e81713772be16f
sunnygao <sunny.gao@liferay.com>
2024-02-05 21:29:46 +0800

    brute force two sum
    gitignore file

--- src/two/sum/Solution.java
+++ src/two/sum/Solution.java
@@ -2,12 +2,15 @@ package two.sum;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0; i < nums.length; i++) {
-            for(int j=0; j< nums.length; j++) {
-                nums[i] +nums[j] = target;
+        for(int i = 0; i < nums.length; i++) {
+
+            for(int j = 0; j< nums.length; j++) {
+
+                if (nums[i] + nums[j] == target)
+                    return new int[] {i, j};
+
             }
-            return int[] i,j;
         }
-        return null;
+        return new int[] {};
     }
 }
